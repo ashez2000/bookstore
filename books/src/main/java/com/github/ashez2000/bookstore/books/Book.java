d7583192dfcbbd0a94c6dbffbb5e5af14e83503f
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2025-08-21 00:43:26 +0530

    Add book entity and repository

---
+++ books/src/main/java/com/github/ashez2000/bookstore/books/Book.java
@@ -0,0 +1,21 @@
+package com.github.ashez2000.bookstore.books;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "books")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String author;
+}
