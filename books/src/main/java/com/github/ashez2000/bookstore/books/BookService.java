462449826548ddd87c9039ea550304ad681b76ac
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2025-08-21 01:14:32 +0530

    Add book service layer

---
+++ books/src/main/java/com/github/ashez2000/bookstore/books/BookService.java
@@ -0,0 +1,34 @@
+package com.github.ashez2000.bookstore.books;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class BookService {
+    private BookRepository bookRepository;
+
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> getBook(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book createBook(CreateBookDto dto) {
+        var book = new Book();
+        book.setTitle(dto.title);
+        book.setDescription(dto.description);
+        book.setTitle(dto.author);
+
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
---
+++ books/src/main/java/com/github/ashez2000/bookstore/books/CreateBookDto.java
@@ -0,0 +1,7 @@
+package com.github.ashez2000.bookstore.books;
+
+public class CreateBookDto {
+    public String title;
+    public String description;
+    public String author;
+}
