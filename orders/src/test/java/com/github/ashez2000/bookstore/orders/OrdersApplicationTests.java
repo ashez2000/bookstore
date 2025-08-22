36a85a7c006777454bbb823783766891b74b987e
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2025-08-22 19:21:13 +0530

    Add orders service

---
+++ orders/src/test/java/com/github/ashez2000/bookstore/orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ashez2000.bookstore.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
