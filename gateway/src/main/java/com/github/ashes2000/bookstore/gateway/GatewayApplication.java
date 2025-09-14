d81c0d51f704db11c19a360cd05f04bb867f36b9
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2025-09-14 16:11:40 +0530

    Add gateway server

---
+++ gateway/src/main/java/com/github/ashes2000/bookstore/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.ashes2000.bookstore.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
