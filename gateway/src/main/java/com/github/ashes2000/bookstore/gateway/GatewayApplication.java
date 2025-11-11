66a26222f1525b1d857e9a05693e38731c065c19
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2025-11-11 23:24:05 +0530

    Refactor gateway service config

--- gateway/src/main/java/com/github/ashes2000/bookstore/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/github/ashes2000/bookstore/gateway/GatewayApplication.java
@@ -2,12 +2,23 @@ package com.github.ashes2000.bookstore.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
+    @Bean
+    public RouteLocator bookstoreRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/api/books/**")
+                        .uri("lb://BOOKS")
+                ).build();
+    }
 }
